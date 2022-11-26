4b968c7e48145429721b9bc0ed6c702264a6fe2c
Daniel Brockhaus <daniel@brockhaus.org>
2022-11-26 20:35:57 +0100

    Cleanup of Public Version

---
+++ sound.h
@@ -0,0 +1,10 @@
+/*
+ * Part of Astonia Client (c) Daniel Brockhaus. Please read license.txt.
+ */
+
+void play_pak_sound(int nr,int vol,int p);
+void sound_mood(void);
+void sound_display(void);
+void sound_gc(void);
+void make_sound_pak(void);
+int init_sound(HWND hwnd);
