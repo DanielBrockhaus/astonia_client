0681e548c57ba99f5962547563b8a5ca61507356
Daniel Brockhaus <daniel@brockhaus.org>
2023-01-19 23:41:42 +0100

    Added some sprites to smoothify list. Bugfixes.

--- sdl.c
+++ sdl.c
@@ -694,8 +694,16 @@ int sdl_load_image_png(struct sdl_image *si,char *filename,zip_t *zip,int smooth
 int do_smoothify(int sprite) {
 
     // TODO: add more to this list
-    if (sprite<=1000) return 1; // GUI
-    if (sprite>=100000) return 1;   // all character sprites
+    if (sprite<=1000) return 1;                     // GUI
+    if (sprite>=10000 && sprite<11000) return 1;    // items
+    if (sprite>=11000 && sprite<12000) return 1;    // coffin, berries, farn, ...
+    if (sprite>=13000 && sprite<14000) return 1;    // bones and towers, ...
+    if (sprite>=16000 && sprite<17000) return 1;    // cameron doors, carts, ...
+    if (sprite>=20025 && sprite<20034) return 1;    // torches
+    if (sprite>=20042 && sprite<20082) return 1;    // torches
+    if (sprite>=20086 && sprite<20119) return 1;    // chests, chairs
+
+    if (sprite>=100000) return 1;                   // all character sprites
 
     return 0;
 }
@@ -1455,7 +1463,7 @@ int sdl_tx_load(int sprite,int sink,int freeze,int scale,int cr,int cg,int cb,in
         }
 
 
-        if (sdlt[stx].flags&SF_DIDMAKE) {
+        if (sdlt[stx].flags&SF_DIDTEX) {
             mem_tex-=sdlt[stx].xres*sdlt[stx].yres*sizeof(uint32_t);
             SDL_DestroyTexture(sdlt[stx].tex);
         } else {
