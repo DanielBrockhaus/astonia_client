a1830aee8c022b666e077e9cc99647d7b5f25775
Mirage <56789444+MirageCode@users.noreply.github.com>
2023-12-26 00:39:39 +0000

    Get memory usage on Windows and Linux for display in gui.c

--- src/game/memory_windows.c
+++ src/game/memory_windows.c
@@ -1,8 +1,15 @@
 #include <windows.h>
+#include <psapi.h>
 
 unsigned long long get_total_system_memory(void) {
     MEMORYSTATUSEX ms;
     ms.dwLength=sizeof(ms);
     GlobalMemoryStatusEx(&ms);
     return ms.ullTotalPhys;
+}
+
+size_t get_memory_usage(void) {
+    PROCESS_MEMORY_COUNTERS mi;
+    GetProcessMemoryInfo(GetCurrentProcess(),&mi,sizeof(mi));
+    return mi.WorkingSetSize;
 }
